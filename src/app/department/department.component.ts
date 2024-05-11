a76098cf7cff0ceb9c797bd6cde4d6f1b0d2e3ef
Shailendra Sharma <er.shailendrasharma@hotmail.com>
2024-05-11 12:48:00 +0530

    first commit

---
+++ src/app/department/department.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department',
+  templateUrl: './department.component.html'
+})
+export class DepartmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
